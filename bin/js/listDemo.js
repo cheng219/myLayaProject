f933d07eee5288496a694a3d8ab678b4f52a0572
dengcheng <369132492@qq.com>
2018-08-16 11:27:54 +0800

    list示例

--- bin/js/listDemo.js
+++ bin/js/listDemo.js
@@ -17,18 +17,22 @@ var demo;
             this.listPage.pos(Laya.stage.width / 2, Laya.stage.height / 2);
             this.listPage.pivot(this.listPage.width / 2, this.listPage.height / 2);
             this.setListData();
+            this.listPage.add.on(Laya.Event.CLICK, this, this.clickAdd);
+            this.listPage.del.on(Laya.Event.CLICK, this, this.clickDel);
         }
         setListData() {
             this.arr = [];
             let str = "list名字";
-            for (let i = 1; i <= 30; i++) {
+            for (let i = 1; i <= 10; i++) {
                 this.arr.push({ listNumber: { text: i }, listName: { text: str + i } });
             }
+            this.listPage._list.vScrollBarSkin = "";
             this.listPage._list.array = this.arr;
             this.listPage._list.renderHandler = new Handler(this, this.listRender);
+            this.listPage._list.mouseHandler = new Handler(this, this.onCheck);
         }
         listRender(cell, index) {
-            console.log("listRender:" + index);
+            //console.log("listRender:"+index);
             if (index > this.arr.length)
                 return;
             let num = this.arr[index];
@@ -36,6 +40,39 @@ var demo;
             listNumber.text = num.listNumber.text;
             let listName = cell.getChildByName("listName");
             listName.text = num.listName.text;
+            let listCheckBox = cell.getChildByName("listCheck");
+            listCheckBox.selected = num.isCheck;
+        }
+        onCheck(e, index) {
+            console.log("onCheck:" + index);
+            if (e.type == Laya.Event.CLICK) {
+                console.log("onCheck CLICK:" + index);
+                if (e.target instanceof laya.ui.CheckBox) {
+                    console.log("onCheck CLICK checkbox:" + index);
+                    let data = this.arr[index];
+                    if (e.target.selected) {
+                        this.listPage._list.setItem(index, { listNumber: { text: data.listNumber.text }, listName: { text: data.listName.text }, isCheck: true });
+                    }
+                    else {
+                        this.listPage._list.setItem(index, { listNumber: { text: data.listNumber.text }, listName: { text: data.listName.text }, isCheck: false });
+                    }
+                }
+            }
+        }
+        clickAdd() {
+            console.log("clickAdd");
+            this.listPage._list.addItem({ listNumber: { text: this.arr.length + 1 }, listName: { text: "增加的" + (this.arr.length + 1) } });
+        }
+        clickDel() {
+            console.log("clickDel");
+            let newArr = [];
+            for (let i = 0; i < this.arr.length; i++) {
+                if (!this.arr[i].isCheck) {
+                    newArr.push(this.arr[i]);
+                }
+            }
+            this.arr = newArr;
+            this.listPage._list.array = this.arr;
         }
     }
     demo.listDemo = listDemo;
--- src/listDemo.ts
+++ src/listDemo.ts
@@ -25,28 +25,76 @@ module demo
             this.listPage.pos(Laya.stage.width / 2,Laya.stage.height / 2);
             this.listPage.pivot(this.listPage.width / 2,this.listPage.height / 2);
             this.setListData();
+
+            this.listPage.add.on(Laya.Event.CLICK,this,this.clickAdd);
+            this.listPage.del.on(Laya.Event.CLICK,this,this.clickDel);
         }
 
         private setListData() : void
         {
             this.arr = [];
             let str:string = "list名字";
-            for(let i:number = 1;i<=30;i++)
+            for(let i:number = 1;i<=10;i++)
             {
                 this.arr.push({listNumber:{text:i},listName:{text:str + i}});
             }
+            this.listPage._list.vScrollBarSkin = "";
             this.listPage._list.array = this.arr;
             this.listPage._list.renderHandler = new Handler(this,this.listRender);
+            this.listPage._list.mouseHandler = new Handler(this,this.onCheck);
         }
         private listRender(cell:Box,index:number):void
         {
-            console.log("listRender:"+index);
+            //console.log("listRender:"+index);
             if(index > this.arr.length)return;
             let num : any = this.arr[index];
             let listNumber:Label = cell.getChildByName("listNumber") as Label;
             listNumber.text = num.listNumber.text;
             let listName:Label = cell.getChildByName("listName") as Label;
             listName.text = num.listName.text;
+            let listCheckBox:laya.ui.CheckBox = cell.getChildByName("listCheck") as laya.ui.CheckBox;
+            listCheckBox.selected = num.isCheck;
+        }
+
+        private onCheck(e:Event,index:number):void
+        {
+            //console.log("onCheck:"+index);
+            if(e.type == Laya.Event.CLICK)
+            {
+                console.log("onCheck CLICK:"+index);
+                if(e.target instanceof laya.ui.CheckBox)
+                {
+                    console.log("onCheck CLICK checkbox:"+index);
+                    let data : any = this.arr[index];
+                    if((e.target as laya.ui.CheckBox).selected)
+                    {
+                        this.listPage._list.setItem(index,{listNumber:{text:data.listNumber.text}, listName:{text:data.listName.text},isCheck:true});
+                    }else
+                    {
+                        this.listPage._list.setItem(index,{listNumber:{text:data.listNumber.text}, listName:{text:data.listName.text},isCheck:false});
+                    }
+                }
+            }
+        }
+
+        private clickAdd() : void
+        {
+            console.log("clickAdd");
+            this.listPage._list.addItem({listNumber:{text:this.arr.length + 1},listName:{text:"增加的" + (this.arr.length + 1)}});
+        }
+        private clickDel() : void
+        {
+            console.log("clickDel");
+            let newArr : Array<any> = [];
+            for(let i : number = 0;i<this.arr.length;i++)
+            {
+                if(!this.arr[i].isCheck)
+                {
+                    newArr.push(this.arr[i]);
+                }
+            }
+            this.arr = newArr;
+            this.listPage._list.array = this.arr;
         }
     }
 }
